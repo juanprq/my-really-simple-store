548c250d8fadacaaa3d3d513a0cc176f5024c286
Juan Pablo Ramírez <juan.ramirez.q@gmail.com>
2015-02-11 16:05:47 -0500

    setup for test environment

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'dotenv-rails', :groups => [:development, :test]
 
 gem 'rails', '4.2.0'
 gem 'pg'
+gem 'sqlite3'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-require 'dotenv'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -23,6 +22,5 @@ module MyReallySimpleStore
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    Dotenv.load File.expand_path("../../.env.#{ENV['RAILS_ENV']}", __FILE__)
   end
 end
