8dfe1a86f4ad33b964c7462d69b59cf3bbcccc60
Juan Pablo Ramírez <juan.ramirez.q@gmail.com>
2015-03-03 14:42:08 -0500

    added filter to clients

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150303155528) do
+ActiveRecord::Schema.define(version: 20150303191110) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,14 @@ ActiveRecord::Schema.define(version: 20150303155528) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "total_points"
+  end
+
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "searchable_id"
+    t.string   "searchable_type"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   create_table "products", force: :cascade do |t|
