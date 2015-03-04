845fd8a3290ed04aab31adc0d32ef83f1a426f03
Juan Pablo Ramírez <juan.ramirez.q@gmail.com>
2015-03-04 16:35:00 -0500

    Implemented controllers methods

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,63 +1,44 @@
 class GamesController < ApplicationController
   before_action :set_game, only: [:show, :edit, :update, :destroy]
 
-  # GET /games
-  # GET /games.json
   def index
-    @games = Game.all
+    @games = Game.all_actives.paginate page: params[:page], per_page: 30
+    flash[:notice] = 'Sin resultados.' if @games.empty?
   end
 
-  # GET /games/1
-  # GET /games/1.json
   def show
   end
 
-  # GET /games/new
   def new
     @game = Game.new
   end
 
-  # GET /games/1/edit
   def edit
   end
 
-  # POST /games
-  # POST /games.json
   def create
     @game = Game.new(game_params)
 
-    respond_to do |format|
-      if @game.save
-        format.html { redirect_to @game, notice: 'Game was successfully created.' }
-        format.json { render :show, status: :created, location: @game }
-      else
-        format.html { render :new }
-        format.json { render json: @game.errors, status: :unprocessable_entity }
-      end
+    if @game.save
+      redirect_to games_path, notice: 'Juego credo con éxito.'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /games/1
-  # PATCH/PUT /games/1.json
   def update
-    respond_to do |format|
-      if @game.update(game_params)
-        format.html { redirect_to @game, notice: 'Game was successfully updated.' }
-        format.json { render :show, status: :ok, location: @game }
-      else
-        format.html { render :edit }
-        format.json { render json: @game.errors, status: :unprocessable_entity }
-      end
+    if @game.update(game_params)
+      redirect_to games_path, notice: 'Juego actualizado con éxito.'
+    else
+      render :edit
     end
   end
 
-  # DELETE /games/1
-  # DELETE /games/1.json
   def destroy
-    @game.destroy
+    @game.inactivate
+    TrashedRecord.create_trash @game
     respond_to do |format|
-      format.html { redirect_to games_url, notice: 'Game was successfully destroyed.' }
-      format.json { head :no_content }
+      redirect_to games_url, notice: 'Juego inactivado con éxito.'
     end
   end
 
