f64508c2d8a26b186c7d7bcf64522b0cba307e77
Juan Pablo Ramírez <juan.ramirez.q@gmail.com>
2015-03-13 16:47:10 -0500

    added model for the relation

---
+++ app/models/client_game.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: client_games
+#
+#  id         :integer          not null, primary key
+#  client_id  :integer
+#  game_id    :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class ClientGame < ActiveRecord::Base
+
+end
